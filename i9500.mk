ff2fc0eb929854583aec4436d65c09d69dc49ba4
KimMinGi99 <m9971@naver.com>
2014-02-15 19:10:42 +0900

    Update i9500.mk
    Add E300K

--- i9500.mk
+++ i9500.mk
@@ -16,7 +16,7 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-LOCAL_PATH := device/samsung/i9500
+LOCAL_PATH := device/samsung/e300k
 
 # overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
@@ -201,4 +201,4 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalv
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # call the proprietary setup
-$(call inherit-product-if-exists, vendor/samsung/i9500/i9500-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/e300k/e300k-vendor.mk)
