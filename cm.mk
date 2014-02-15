490bb1939874849c9dea49f62f6fcf43a9b5d729
KimMinGi99 <m9971@naver.com>
2014-02-15 19:13:04 +0900

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/samsung/i9500/full_i9500.mk)
+$(call inherit-product, device/samsung/i9500/full_e300k.mk)
 
 # Inherit some common CM stuff
 $(call inherit-product, vendor/cm/config/gsm.mk)
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_i9500
-PRODUCT_DEVICE := i9500
+PRODUCT_NAME := cm_e300k
+PRODUCT_DEVICE := e300k
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ja3gxx TARGET_DEVICE=ja3g BUILD_FINGERPRINT="samsung/ja3gxx/ja3g:4.3/JSS15J/I9500XXUEMK8:user/release-keys" PRIVATE_BUILD_DESC="ja3gxx-user 4.3 JSS15J I9500XXUEMK8 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jaltektt TARGET_DEVICE=jaltektt BUILD_FINGERPRINT="samsung/ja3gxx/ja3g:4.3/JSS15J/I9500XXUEMK8:user/release-keys" PRIVATE_BUILD_DESC="ja3gxx-user 4.3 JSS15J I9500XXUEMK8 release-keys"
